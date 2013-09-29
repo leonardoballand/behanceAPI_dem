fb72d9f04dc6012dd08eb23831f652110ba9da50
digitalz <leodsirs@gmail.com>
2013-09-29 20:59:18 -0300

    Initial commit.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+	def demo
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Behance::Application.config.secret_token = '8bee458468bad9f730022c0e7a3e1125d0092e27855a3d18f289ef6bbfb0ba12eb827f38c7a6f0444a390994a2128094989d6c5fc3aa45c459d361cce14bf4ed'
